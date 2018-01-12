2b2b53568f72da4356e5e9f5c1545ab7e4c251fd
Adam Giese <adam.giese@underarmour.com>
2018-01-12 10:06:20 -0600

    Add details to grep slides
    Update styling
    Remove example slides in favor of the following:
    - Default usage
    - Useful flags
    - Using pathspec

--- src/js/components/Content/grep/index.jsx
+++ src/js/components/Content/grep/index.jsx
@@ -3,15 +3,15 @@ import React, { Fragment } from 'react';
 import Code from '../utils/code.jsx';
 /* eslint-ensable no-unused-vars */
 
-const GrepTitle = () => (
+const Title = () => (
   <Fragment>
     <h2>git grep</h2>
   </Fragment>
 );
 
-const GrepAdvantages = () => (
+const Advantages = () => (
   <article>
-    <h3>Why git grep?</h3>
+    <h3>why git grep?</h3>
     <ul>
       <li>By default, only searches files tracked by git</li>
       <li>Allows searching of previous commits</li>
@@ -20,43 +20,51 @@ const GrepAdvantages = () => (
   </article>
 );
 
-const GrepExamples = () => (
-  <div>
-    <h3>Some Examples</h3>
-    <ul className="examples">
+const Default = () => (
+  <article>
+    <h3>default usage</h3>
+    <Code caption="Find all tracked occurances of 'TODO' within the current working directory">
+      > git grep TODO
+    </Code>
+  </article>
+);
+
+const UsefulFlags = () => (
+  <article>
+    <h3>an incomplete list of <br />useful grep flags</h3>
+    <ul style={ { listStyle: 'none' } }>
       <li>
-        <Code caption="Find all tracked occurances of 'TODO' within the CWD">
-          > git grep TODO
-        </Code>
+        <Code caption="In addition to default, include files not managed by git, including those which are untracked and ignored.">--no-index</Code>
       </li>
       <li>
-        <Code caption="Find ALL occurances of 'TODO' within the CWD">
-          > git grep --no-index TODO
-        </Code>
+        <Code caption="In addition to default, include files not tracked by git.">--untracked</Code>
       </li>
       <li>
-        <Code caption="Find tracked and untracked, but not ignored, occurances of 'TODO' within the CWD">
-          > git grep --untracked TODO
-        </Code>
-      </li>
-      <li>
-        <Code caption="Find tracked and untracked, but not ignored, occurances of 'TODO' within the `/src` directory">
-          > git grep --untracked TODO -- src
-        </Code>
-      </li>
-      <li>
-        <Code caption="Find tracked and untracked, but not ignored, occurances of 'TODO' within the project">
-          > git grep --untracked TODO -- `git rev-parse --show-toplevel`
-        </Code>
+        <Code caption="Ignore Case">-i, --ignore-case</Code>
       </li>
     </ul>
-  </div>
+  </article>
+);
+
+const Path = () => (
+  <article>
+    <h3>including a pathspec</h3>
+    <ul style={ { listStyle: 'none' } }>
+      <li><Code caption="a file">> git grep TODO -- 'README.md'</Code></li>
+      <li><Code caption="a directory">> git grep TODO -- 'src'</Code></li>
+      <li><Code caption="wildcards">> git grep TODO -- '**/*.js'</Code></li>
+      <li><Code caption="from top of directory, regardless of current working directory">> git grep TODO -- ':(top)'</Code></li>
+      <li><Code caption="combo!">> git grep TODO -- ':(top)src/**/*.js'</Code></li>
+    </ul>
+  </article>
 );
 
 const slides = [
-  <GrepTitle />,
-  <GrepAdvantages />,
-  <GrepExamples />,
+  <Title />,
+  <Advantages />,
+  <Default />,
+  <UsefulFlags />,
+  <Path />,
 ];
 
 export default slides;
