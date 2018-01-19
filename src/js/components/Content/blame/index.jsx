221c9af49f452d99878731d6632cd003de1a63f1
Adam Giese <adam.giese@underarmour.com>
2018-01-19 09:23:03 -0600

    Add blame overview

--- src/js/components/Content/blame/index.jsx
+++ src/js/components/Content/blame/index.jsx
@@ -1,15 +1,32 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Code from '../utils/code.jsx';
 /* eslint-ensable no-unused-vars */
 
-const BlameTitle = () => (
-  <div>
-    <h2><code>git blame</code></h2>
-  </div>
+const Title = () => (
+  <h2>git blame</h2>
+);
+
+const Usage = () => (
+  <article>
+    <Code caption="Blames every line in a given file">
+      > git blame README.md
+    </Code>
+    <Code caption="Blames lines in absolute range in a given file">
+      > git blame -L4,6 README.md
+    </Code>
+    <Code caption="Blames lines in range relative to a RegEx in a given file">
+      > git blame -L'/TODO/',+10 README.md
+    </Code>
+    <Code caption="Blames lines in file at a specific commit">
+      > git blame 1b180ec8e^ README.md
+    </Code>
+  </article>
 );
 
 const slides = [
-  <BlameTitle />,
+  <Title />,
+  <Usage />,
 ];
 
 export default slides;
