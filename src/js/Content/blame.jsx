836b8cfbde83ff4f67c9f831dbc0c0b660606da7
Adam Giese <adam.giese@underarmour.com>
2018-06-18 10:41:40 -0500

    Rewrite git blame to standardize

--- src/js/Content/blame.jsx
+++ src/js/Content/blame.jsx
@@ -3,35 +3,27 @@ import React, { Fragment } from 'react';
 import Code from 'Utils/Code.jsx';
 /* eslint-ensable no-unused-vars */
 
-const Title = () => (
-  <h2>git blame</h2>
-);
-
-const Usage = () => (
-  <article>
-    <Code caption="Blames every line in a given file">
-      $ git blame README.md
-    </Code>
-    <Code caption="Blames lines in absolute range in a given file">
-      $ git blame -L4,6 README.md
-    </Code>
-    <Code caption="Blames lines in range relative to a RegEx in a given file">
-      $ git blame -L'/TODO/',+10 README.md
-    </Code>
-    <Code caption="Blames lines in file at a specific commit">
-      $ git blame 1b180ec8e^ README.md
-    </Code>
-  </article>
-);
-
 export default () => (
   <Fragment>
     <section>
-      <Title />
+      <h2>git blame</h2>
     </section>
 
     <section>
-      <Usage />
+      <article>
+        <Code caption="Blames every line in a given file">
+          $ git blame README.md
+        </Code>
+        <Code caption="Blames lines in absolute range in a given file">
+          $ git blame -L4,6 README.md
+        </Code>
+        <Code caption="Blames lines in range relative to a RegEx in a given file">
+          $ git blame -L'/TODO/',+10 README.md
+        </Code>
+        <Code caption="Blames lines in file at a specific commit">
+          $ git blame 1b180ec8e^ README.md
+        </Code>
+      </article>
     </section>
   </Fragment>
 );
